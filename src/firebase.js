c70f2dcf62bc055c4df4735fc12e8af0f31ed1cc
Rustem Abdulaev <rustem.abdulaev@gmail.com>
2023-03-03 20:21:02 +0300

    first commit to deploy 3

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyA9EViL5LYJytJE08NWzRbKWF4tW6hs_fY',
-    authDomain: 'noname--digital.firebaseapp.com',
-    projectId: 'noname--digital',
-    storageBucket: 'noname--digital.appspot.com',
-    messagingSenderId: '830959075360',
-    appId: '1:830959075360:web:e1aa43847d0dd27685fc45'
+    apiKey: "AIzaSyA9EViL5LYJytJE08NWzRbKWF4tW6hs_fY",
+    authDomain: "noname--digital.firebaseapp.com",
+    projectId: "noname--digital",
+    storageBucket: "noname--digital.appspot.com",
+    messagingSenderId: "830959075360",
+    appId: "1:830959075360:web:e1aa43847d0dd27685fc45"
 };
 
 const app = initializeApp(firebaseConfig);
