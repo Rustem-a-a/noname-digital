2168a2142821a71e1eeaa5623dd76b5dfe0ccaaa
Rustem Abdulaev <rustem.abdulaev@gmail.com>
2023-03-03 21:07:46 +0300

    first commit to deploy 4

--- src/index.js
+++ src/index.js
@@ -10,17 +10,9 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Auth0Provider
-        domain='dev-qik4mc5c5tm3xcn3.us.auth0.com'
-        clientId='ZP9wtBYYqDKROlrzG6RhbDAxwvWRZcYm'
-        authorizationParams={{
-            redirect_uri: window.location.origin
-        }}
-    >
         <Provider store={store}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
         </Provider>
-    </Auth0Provider>
 );
